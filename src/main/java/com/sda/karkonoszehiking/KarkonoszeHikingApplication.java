3ac43b30ed26648200dad1db17f9eff2e877153c
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2023-07-17 20:38:34 +0200

    git init

---
+++ src/main/java/com/sda/karkonoszehiking/KarkonoszeHikingApplication.java
@@ -0,0 +1,13 @@
+package com.sda.karkonoszehiking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarkonoszeHikingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarkonoszeHikingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/karkonoszehiking/KarkonoszeHikingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.karkonoszehiking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarkonoszeHikingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
